c9987de2a082b376c1bffa2ada6ab768e47eeaf5
Dusan Rajcevic <dusanrajcevic@users.noreply.github.com>
2025-09-12 16:18:32 +0200

    Reorder import naredbe u FakultetTest klasi

--- zadatak1/tests/fakultet/FakultetTest.java
+++ zadatak1/tests/fakultet/FakultetTest.java
@@ -1,10 +1,10 @@
 package fakultet;
 
 import org.junit.jupiter.api.BeforeEach;
-import osoba.Osoba;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import osoba.Osoba;
 
 import static org.junit.jupiter.api.Assertions.*;
 
