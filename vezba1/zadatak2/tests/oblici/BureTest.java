ce768b2aa816523ce2dd8e4db04cc5d1e1928972
Dusan Rajcevic <dusanrajcevic@users.noreply.github.com>
2025-09-13 15:25:15 +0200

    Test za sortiranje buradi po nerastucem poretku

--- vezba1/zadatak2/tests/oblici/BureTest.java
+++ vezba1/zadatak2/tests/oblici/BureTest.java
@@ -1,6 +1,11 @@
 package oblici;
 
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -34,5 +39,17 @@ public class BureTest {
         Bure b = new Bure(10, 20);
         String expectedString = String.format("%d. Bure P = %.2f, V = %.2f", b.getId(), b.povrsina(), b.zapremina());
         assertEquals(expectedString, b.toString());
+    }
+
+    @Test
+    public void testSortiranjeBure() {
+        Bure b1 = new Bure(10, 20);
+        Bure b2 = new Bure(15, 25);
+        Bure b3 = new Bure(5, 30);
+        List<Bure> bureList = new ArrayList<>(List.of(b1, b2, b3));
+        Collections.sort(bureList);
+        assertEquals(b3, bureList.get(0));
+        assertEquals(b1, bureList.get(1));
+        assertEquals(b2, bureList.get(2));
     }
 }
