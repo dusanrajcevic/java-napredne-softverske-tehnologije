7bff44746f8bdd23896b9489bd90b946c0e06967
Dusan Rajcevic <dusanrajcevic@users.noreply.github.com>
2025-09-13 15:21:00 +0200

    Promena assert -> assertTrue zbog jasnijih poruka u slucaju greske

--- vezba1/zadatak2/tests/oblici/BureTest.java
+++ vezba1/zadatak2/tests/oblici/BureTest.java
@@ -2,6 +2,7 @@ package oblici;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class BureTest {
 
@@ -9,23 +10,23 @@ public class BureTest {
     public void testKreiranjeBure() {
         Bure b1 = new Bure(10, 20);
         Bure b2 = new Bure(15, 25);
-        assert b1.getId() > 0;
-        assert b2.getId() > 0;
-        assert b1.getId() != b2.getId();
+        assertTrue(b1.getId() > 0);
+        assertTrue(b2.getId() > 0);
+        assertTrue(b1.getId() != b2.getId());
     }
 
     @Test
     public void testPovrsina() {
         Bure b = new Bure(10, 20);
         double expectedPovrsina = 2 * Math.pow(5, 2) * Math.PI + 2 * 5 * Math.PI * 20;
-        assert Math.abs(expectedPovrsina - b.povrsina()) < 0.0001;
+        assertTrue(Math.abs(expectedPovrsina - b.povrsina()) < 0.0001);
     }
 
     @Test
     public void testZapremina() {
         Bure b = new Bure(10, 20);
         double expectedZapremina = Math.pow(5, 2) * Math.PI * 20;
-        assert Math.abs(expectedZapremina - b.zapremina()) < 0.0001;
+        assertTrue(Math.abs(expectedZapremina - b.zapremina()) < 0.0001);
     }
 
     @Test
