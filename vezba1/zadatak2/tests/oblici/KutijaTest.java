e16b98869ade79fb999927ed2f8a31c90c9da4e9
Dusan Rajcevic <dusanrajcevic@users.noreply.github.com>
2025-09-13 20:57:36 +0200

    Refactor KutijaTest

--- vezba1/zadatak2/tests/oblici/KutijaTest.java
+++ vezba1/zadatak2/tests/oblici/KutijaTest.java
@@ -1,5 +1,6 @@
 package oblici;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -10,46 +11,47 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class KutijaTest {
+    private Kutija kutija;
+
+    @BeforeEach
+    public void setUp() {
+        kutija = new Kutija(10, 20, 30);
+    }
 
     @Test
     public void testKreiranjeKutije() {
-        Kutija k1 = new Kutija(10, 20, 30);
-        Kutija k2 = new Kutija(20, 30, 40);
-        assertTrue(k1.getId() > 0);
-        assertTrue(k2.getId() > 0);
-        assertTrue(k1.getId() != k2.getId());
+        Kutija kutija2 = new Kutija(20, 30, 40);
+        assertTrue(kutija.getId() > 0);
+        assertTrue(kutija2.getId() > 0);
+        assertTrue(kutija.getId() != kutija2.getId());
     }
 
     @Test
     public void testPovrsina() {
-        Kutija k = new Kutija(10, 20, 30);
         double expectedPovrsina = 2 * (10 * 20 + 10 * 30 + 20 * 30);
-        assertTrue(Math.abs(expectedPovrsina - k.povrsina()) < 0.0001);
+        assertTrue(Math.abs(expectedPovrsina - kutija.povrsina()) < 0.0001);
     }
 
     @Test
     public void testZapremina() {
-        Kutija k = new Kutija(10, 20, 30);
         double expectedZapremina = 10 * 20 * 30;
-        assertTrue(Math.abs(expectedZapremina - k.zapremina()) < 0.0001);
+        assertTrue(Math.abs(expectedZapremina - kutija.zapremina()) < 0.0001);
     }
 
     @Test
     public void testToStringFormat() {
-        Kutija k = new Kutija(10, 20, 30);
-        String expectedString = String.format("%d. Kutija P = %.2f, V = %.2f", k.getId(), k.povrsina(), k.zapremina());
-        assertEquals(expectedString, k.toString());
+        String expectedString = String.format("%d. Kutija P = %.2f, V = %.2f", kutija.getId(), kutija.povrsina(), kutija.zapremina());
+        assertEquals(expectedString, kutija.toString());
     }
 
     @Test
     public void testSortiranjeBure() {
-        Kutija k1 = new Kutija(10, 20, 30);
-        Kutija k2 = new Kutija(15, 25, 35);
-        Kutija k3 = new Kutija(5, 10, 20);
-        List<Kutija> kutije = new ArrayList<>(List.of(k1, k2, k3));
+        Kutija kutija2 = new Kutija(15, 25, 35);
+        Kutija kutija3 = new Kutija(5, 10, 20);
+        List<Kutija> kutije = new ArrayList<>(List.of(kutija, kutija2, kutija3));
         Collections.sort(kutije);
-        assertEquals(k3, kutije.get(0));
-        assertEquals(k1, kutije.get(1));
-        assertEquals(k2, kutije.get(2));
+        assertEquals(kutija3, kutije.get(0));
+        assertEquals(kutija, kutije.get(1));
+        assertEquals(kutija2, kutije.get(2));
     }
 }
