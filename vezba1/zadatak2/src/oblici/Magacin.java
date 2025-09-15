64eb0c35a1fcb20508504cbc728dacb2cd8c30f0
Dusan Rajcevic <dusanrajcevic@users.noreply.github.com>
2025-09-15 02:15:06 +0200

    Refactor ukupnaPovrsinaKontejneraUmagacinu i getKontejnerIndex u klasi Magacin v1z2

--- vezba1/zadatak2/src/oblici/Magacin.java
+++ vezba1/zadatak2/src/oblici/Magacin.java
@@ -3,6 +3,7 @@ package oblici;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class Magacin {
     private final double povrsina;
@@ -30,7 +31,7 @@ public class Magacin {
     }
 
     public void ukloniKontejner(int id) {
-        int i = getKontejner(id);
+        int i = getKontejnerIndex(id);
 
         if (i == -1) {
             throw new IllegalArgumentException("Nepostojeci kontejner!");
@@ -49,27 +50,19 @@ public class Magacin {
     {
         StringBuilder sb = new StringBuilder();
         sb.append(String.format("Magacin, P = %s\n", povrsina));
-        for (Kontejner k : kontejneri) {
-            sb.append(k).append("\n");
-        }
+        kontejneri.forEach(k -> sb.append(k).append("\n"));
         return sb.toString();
     }
 
     private double ukupnaPovrsinaKontejneraUmagacinu()
     {
-        double ukupnaPovrsina = 0;
-        for (Kontejner k : kontejneri) {
-            ukupnaPovrsina += k.povrsina();
-        }
-        return ukupnaPovrsina;
+        return kontejneri.stream().mapToDouble(Kontejner::povrsina).sum();
     }
 
-    private int getKontejner(int id) {
-        for (int i = 0; i < kontejneri.size(); i++) {
-            if (kontejneri.get(i).getId() == id) {
-                return i;
-            }
-        }
-        return -1;
+    private int getKontejnerIndex(int id) {
+        return IntStream.range(0, kontejneri.size())
+                .filter(i -> kontejneri.get(i).getId() == id)
+                .findFirst()
+                .orElse(-1);
     }
 }
