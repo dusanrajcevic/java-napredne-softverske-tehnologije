aa6f39bd3e412248ddf9963a4b53613c5ecf12e9
Dusan Rajcevic <dusanrajcevic@users.noreply.github.com>
2025-09-16 00:12:46 +0200

    Dodavanje knjige u biblioteku

--- vezba1/zadatak5/src/biblioteka/Biblioteka.java
+++ vezba1/zadatak5/src/biblioteka/Biblioteka.java
@@ -4,6 +4,7 @@ import knjiga.Knjiga;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class Biblioteka {
     private final String naziv;
@@ -15,4 +16,10 @@ public class Biblioteka {
         this.clanovi = new ArrayList<>();
         this.knjige = new ArrayList<>();
     }
+
+    public Biblioteka dodajKnjigu(Knjiga knjiga) {
+        this.knjige.add(knjiga);
+        return this;
+    }
+
 }
--- vezba1/zadatak5/tests/biblioteka/BibliotekaTest.java
+++ vezba1/zadatak5/tests/biblioteka/BibliotekaTest.java
@@ -1,5 +1,6 @@
 package biblioteka;
 
+import knjiga.Knjiga;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
@@ -9,6 +10,16 @@ public class BibliotekaTest {
     public void testKreiranjeBiblioteke() {
         assertDoesNotThrow(() -> {
             new Biblioteka("Narodna biblioteka");
+        });
+    }
+
+    @Test
+    public void testDodavanjeKnjige() {
+        assertDoesNotThrow(() -> {
+            Biblioteka biblioteka = new Biblioteka("Narodna biblioteka");
+            Knjiga knjiga1 = new Knjiga("Naslov knjige", "Autor knjige", 2020);
+            Knjiga knjiga2 = new Knjiga("Naslov knjige 2", "Autor knjige 2", 2022);
+            biblioteka.dodajKnjigu(knjiga1).dodajKnjigu(knjiga2);
         });
     }
 }
