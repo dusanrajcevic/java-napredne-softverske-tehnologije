ebdc01ff1ac503b5a72c9efde3c5818260c137af
Dusan Rajcevic <dusanrajcevic@users.noreply.github.com>
2025-09-15 05:15:02 +0200

    Klasa Fudbaler sa konstruktorom, getterima i jedinicnim testovima

---
+++ vezba1/zadatak4/src/sport/Fudbaler.java
@@ -0,0 +1,16 @@
+package sport;
+
+import osobe.Igrac;
+
+public class Fudbaler extends Igrac {
+    private final String pozicija;
+
+    public Fudbaler(String imePrezime, int godine, String pozicija) {
+        super(imePrezime, godine);
+        this.pozicija = pozicija;
+    }
+
+    public String getPozicija() {
+        return pozicija;
+    }
+}
---
+++ vezba1/zadatak4/tests/sport/FudbalerTest.java
@@ -0,0 +1,15 @@
+package sport;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FudbalerTest {
+    @Test
+    public void testKreiranjeFudbalera() {
+        Fudbaler f = new Fudbaler("Petar Petrovic", 22, "napadac");
+        assertEquals("Petar Petrovic", f.getImePrezime());
+        assertEquals(22, f.getGodine());
+        assertEquals("napadac", f.getPozicija());
+    }
+}
+
