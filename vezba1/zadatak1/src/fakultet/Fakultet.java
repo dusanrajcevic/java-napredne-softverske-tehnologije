108e35dd25536b0186fea0b31213eeda50323e84
Dusan Rajcevic <dusanrajcevic@users.noreply.github.com>
2025-09-15 02:17:02 +0200

    Nov red u ToString klasa Fakultet v1z1

--- vezba1/zadatak1/src/fakultet/Fakultet.java
+++ vezba1/zadatak1/src/fakultet/Fakultet.java
@@ -39,7 +39,8 @@ public class Fakultet {
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
-        sb.append("Fakultet: ").append(naziv).append("\n")
+        sb.append("Fakultet: ")
+                .append(naziv).append("\n")
                 .append("- Dekan: ").append(dekan).append("\n")
                 .append("--- Studenti:\n");
         studenti.forEach(
