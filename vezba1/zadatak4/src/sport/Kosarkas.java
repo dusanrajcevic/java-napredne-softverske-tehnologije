27c99a6fcefd183e43844c2867b734c7e5bb5bdd
Dusan Rajcevic <dusanrajcevic@users.noreply.github.com>
2025-09-15 05:23:45 +0200

    Klasa Kosarkas sa konstruktorom, getterima i jedinicnim testovima

---
+++ vezba1/zadatak4/src/sport/Kosarkas.java
@@ -0,0 +1,17 @@
+package sport;
+
+import osobe.Igrac;
+
+public class Kosarkas extends Igrac {
+    private final int visina;
+
+    public Kosarkas(String imePrezime, int godine, int visina) {
+        super(imePrezime, godine);
+        this.visina = visina;
+    }
+
+    public int getVisina() {
+        return visina;
+    }
+}
+
---
+++ vezba1/zadatak4/tests/sport/KosarkasTest.java
@@ -0,0 +1,15 @@
+package sport;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class KosarkasTest {
+    @Test
+    public void testKreiranjeKosarkasa() {
+        Kosarkas k = new Kosarkas("Jovan Jovanovic", 27, 198);
+        assertEquals("Jovan Jovanovic", k.getImePrezime());
+        assertEquals(27, k.getGodine());
+        assertEquals(198, k.getVisina());
+    }
+}
+
