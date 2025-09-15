3bb4e5577532b2c79b50777d7130b4c314276522
Dusan Rajcevic <dusanrajcevic@users.noreply.github.com>
2025-09-15 07:37:23 +0200

    Testiranje rada klasa u Main-u za v1z4

--- vezba1/zadatak4/src/Main.java
+++ vezba1/zadatak4/src/Main.java
@@ -1,5 +1,28 @@
+import sport.Fudbaler;
+import sport.Kosarkas;
+import sport.Klub;
+
 public class Main {
     public static void main(String[] args) {
+        Klub klub = new Klub("Crvena Zvezda", "fudbal");
+        Fudbaler f1 = new Fudbaler("Marko Markovic", 25, "odbrambeni");
+        Fudbaler f2 = new Fudbaler("Petar Petrovic", 22, "napadac");
+        Kosarkas k1 = new Kosarkas("Jovan Jovanovic", 27, 198);
 
+        klub.dodajIgraca(f1);
+        klub.dodajIgraca(f2);
+        klub.dodajIgraca(k1);
+
+        System.out.println("--- Klub bez sortiranja ---");
+        System.out.print(klub);
+
+        klub.sortirajPoGodinamaNeopadajuce();
+        System.out.println("--- Klub sa sortiranim igracima ---");
+        System.out.print(klub);
+
+        klub.ukloniIgraca(f2.getId());
+        System.out.println("--- Klub nakon uklanjanja igraca ---");
+        System.out.print(klub);
     }
 }
+
