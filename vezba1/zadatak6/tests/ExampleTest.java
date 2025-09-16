0db1fd75e9f758ffb4f216f1d1eb60bdbe7dd045
Dusan Rajcevic <dusanrajcevic@users.noreply.github.com>
2025-09-16 19:34:44 +0200

    JUnit biblioteka

---
+++ vezba1/zadatak6/tests/ExampleTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ExampleTest {
+    @Test
+    public void test() {
+        int a = 1;
+        assertEquals(1, a);
+    }
+}
