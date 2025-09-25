8ce83b1a14976f2c385b85ffe4c827dd0161d7d5
Dusan Rajcevic <dusanrajcevic@users.noreply.github.com>
2025-09-25 23:59:49 +0200

    Konverzija iz Java 23 -> Java 25 z3v1

--- vezba1/zadatak3/src/Main.java
+++ vezba1/zadatak3/src/Main.java
@@ -4,42 +4,40 @@ import tovar.Prtljag;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Main {
-    public static void main(String[] args) {
-        TeretnoVozilo vozilo = new TeretnoVozilo(1000, "Zastava");
-        List<Prtljag> prtljazi = new ArrayList<>(
+void main() {
+    TeretnoVozilo vozilo = new TeretnoVozilo(1000, "Zastava");
+    List<Prtljag> prtljazi = new ArrayList<>(
             List.of(
-                new Prtljag("Dzak", 650),
-                new Prtljag("Torba", 75),
-                new Prtljag("Dzak", 400),
-                new Prtljag("Kesa", 300),
-                new Prtljag("Ranac", 45)
+                    new Prtljag("Dzak", 650),
+                    new Prtljag("Torba", 75),
+                    new Prtljag("Dzak", 400),
+                    new Prtljag("Kesa", 300),
+                    new Prtljag("Ranac", 45)
             )
-        );
+    );
 
-        prtljazi.forEach(v -> {
-            try {
-                vozilo.dodajPrtljag(v);
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-            }
-        });
-
-        System.out.println(vozilo);
-
+    prtljazi.forEach(v -> {
         try {
-            vozilo.istovariPrtljag(4);
+            vozilo.dodajPrtljag(v);
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
         }
+    });
 
-        try {
-            vozilo.dodajPrtljag(new Prtljag("Kesa", 29));
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        }
-        System.out.println("---");
-        System.out.println("Nakon dodavanja kese: ");
-        System.out.println(vozilo);
+    System.out.println(vozilo);
+
+    try {
+        vozilo.istovariPrtljag(4);
+    } catch (IllegalArgumentException e) {
+        System.out.println(e.getMessage());
     }
+
+    try {
+        vozilo.dodajPrtljag(new Prtljag("Kesa", 29));
+    } catch (IllegalArgumentException e) {
+        System.out.println(e.getMessage());
+    }
+    System.out.println("---");
+    System.out.println("Nakon dodavanja kese: ");
+    System.out.println(vozilo);
 }
