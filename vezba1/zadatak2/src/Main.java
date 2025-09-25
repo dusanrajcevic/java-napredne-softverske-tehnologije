889160fd27a03e4d97396e62927a353204a0e346
Dusan Rajcevic <dusanrajcevic@users.noreply.github.com>
2025-09-25 23:57:18 +0200

    Konverzija iz Java 23 -> Java 25 z2v1

--- vezba1/zadatak2/src/Main.java
+++ vezba1/zadatak2/src/Main.java
@@ -1,49 +1,47 @@
 import oblici.*;
 
-public class Main {
-    public static void main(String[] args) {
-        Bure bure1 = new Bure(10, 20);
-        Bure bure2 = new Bure(12, 18);
-        Kutija kutija1 = new Kutija(5, 6, 7);
-        Kutija kutija2 = new Kutija(8, 9, 10);
-        Magacin magacin = new Magacin(2500);
+void main() {
+    Bure bure1 = new Bure(10, 20);
+    Bure bure2 = new Bure(12, 18);
+    Kutija kutija1 = new Kutija(5, 6, 7);
+    Kutija kutija2 = new Kutija(8, 9, 10);
+    Magacin magacin = new Magacin(2500);
 
-        try {
-            magacin.dodajKontejner(bure1)
-                   .dodajKontejner(bure2)
-                   .dodajKontejner(kutija1)
-                   .dodajKontejner(kutija2);
-        } catch (IllegalStateException e) {
-            System.out.println(e.getMessage());
-        }
-
-        Bure burePreveliko = new Bure(100, 100);
-        try {
-            magacin.dodajKontejner(burePreveliko);
-        } catch (IllegalStateException e) {
-            System.out.println(e.getMessage());
-        }
-
-        System.out.println("Magacin (pre uklanjanja):");
-        System.out.println(magacin);
-        try {
-            magacin.ukloniKontejner(bure1.getId());
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        }
-
-        try {
-            magacin.ukloniKontejner(-1);
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        }
-
-
-        System.out.println("Magacin (nakon uklanjanja):");
-        System.out.println(magacin);
-
-        magacin.sortirajKontejnerePoPovrsiniNerastuce();
-        System.out.println("Magacin (nakon sortiranja):");
-        System.out.println(magacin);
+    try {
+        magacin.dodajKontejner(bure1)
+                .dodajKontejner(bure2)
+                .dodajKontejner(kutija1)
+                .dodajKontejner(kutija2);
+    } catch (IllegalStateException e) {
+        System.out.println(e.getMessage());
     }
+
+    Bure burePreveliko = new Bure(100, 100);
+    try {
+        magacin.dodajKontejner(burePreveliko);
+    } catch (IllegalStateException e) {
+        System.out.println(e.getMessage());
+    }
+
+    System.out.println("Magacin (pre uklanjanja):");
+    System.out.println(magacin);
+    try {
+        magacin.ukloniKontejner(bure1.getId());
+    } catch (IllegalArgumentException e) {
+        System.out.println(e.getMessage());
+    }
+
+    try {
+        magacin.ukloniKontejner(-1);
+    } catch (IllegalArgumentException e) {
+        System.out.println(e.getMessage());
+    }
+
+
+    System.out.println("Magacin (nakon uklanjanja):");
+    System.out.println(magacin);
+
+    magacin.sortirajKontejnerePoPovrsiniNerastuce();
+    System.out.println("Magacin (nakon sortiranja):");
+    System.out.println(magacin);
 }
