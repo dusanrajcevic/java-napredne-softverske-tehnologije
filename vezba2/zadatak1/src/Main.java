af3ea3f58ac88a577000a8cccb731046065c1562
Dusan Rajcevic <dusanrajcevic@users.noreply.github.com>
2025-09-29 12:51:07 +0200

    Glavni program

--- vezba2/zadatak1/src/Main.java
+++ vezba2/zadatak1/src/Main.java
@@ -1,3 +1,16 @@
-void main() {
+import festival.Festival;
 
+void main() {
+    Festival f = new Festival("Exit", LocalDate.of(2025, 12, 31));
+    f.dodajNastup("Parni Valjak", 90);
+    f.dodajNastup("Dubioza Kolektiv", 75);
+    f.dodajNastup("Riblja Corba", 80);
+
+    System.out.println("---Pre sortiranja:---");
+    System.out.println(f);
+
+    System.out.println("\n\n---Nakon sortiranja po trajanju:---");
+    f.sortirajNastupePoTrajanju();
+    System.out.println(f);
+    System.out.println(f.prikaziRaspored(20, 0, 10));
 }
