91da2e55ab695a919d5bd4daf85bb95ad916e395
Dusan Rajcevic <dusanrajcevic@users.noreply.github.com>
2025-09-30 04:57:15 +0200

    Testiranje rada klasa u Mainu v2z2

--- vezba2/zadatak2/src/Main.java
+++ vezba2/zadatak2/src/Main.java
@@ -1,3 +1,10 @@
-void main() {
+import konferencija.Konferencija;
 
+void main() {
+    Konferencija konf = new Konferencija("IT Trends", LocalDate.of(2025, 12, 5));
+    konf.dodajPredavanje("Veštačka inteligencija", "Dr Marko Marković", 45);
+    konf.dodajPredavanje("Sajber bezbednost", "Dr Petar Petrović", 50);
+    konf.dodajPredavanje("Kvantno računarstvo", "Dr Ivana Ilić", 60);
+    System.out.print(konf);
+    System.out.println(konf.ispisiAgendu(10, 0, 0));
 }
