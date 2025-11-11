45cbcecfc88eee7b81fb8a100c3e328fee8f6625
Dusan Rajcevic <dusanrajcevic@users.noreply.github.com>
2025-11-11 13:29:10 +0100

    Metoda toString za NumerickiMagacin sa jedinicnom testom.

--- vezba3/zadatak1/src/kolekcija/NumerickiMagacin.java
+++ vezba3/zadatak1/src/kolekcija/NumerickiMagacin.java
@@ -8,5 +8,15 @@ public class NumerickiMagacin<T extends Number> extends Magacin<T> {
         }
         return suma;
     }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Numericki magacin (zbir=").append(zbir()).append("):\n");
+        for (Kutija<T> k : kutije) {
+            sb.append("    ").append(k).append("\n");
+        }
+        return sb.toString();
+    }
 }
 
--- vezba3/zadatak1/tests/kolekcija/NumerickiMagacinTest.java
+++ vezba3/zadatak1/tests/kolekcija/NumerickiMagacinTest.java
@@ -13,4 +13,15 @@ public class NumerickiMagacinTest {
         magacin.dodajKutiju(new Kutija<>(30));
         assertEquals(60.0, magacin.zbir(), 0.001);
     }
+
+    @Test
+    public void testToString() {
+        NumerickiMagacin<Double> magacin = new NumerickiMagacin<>();
+        magacin.dodajKutiju(new Kutija<>(10.5));
+        magacin.dodajKutiju(new Kutija<>(20.5));
+        String expected = "Numericki magacin (zbir=31.0):\n" +
+                          "    1. 10.5\n" +
+                          "    2. 20.5\n";
+        assertEquals(expected, magacin.toString());
+    }
 }
